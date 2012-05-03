a0d14e9c53f632d55787a08bef0ac2df2169b9e3
Rogan Creswick <creswick@gmail.com>
2012-05-02 17:52:32 -0700

    initial revision

---
+++ 0.11/condfields/htdocs/condfields.js
@@ -0,0 +1,152 @@
+// var condfields = {};
+
+
+// #for type, fields in condfields.types.iteritems()
+// condfields['$type'] = {};
+// #for field, val in fields.iteritems()
+// condfields['$type']['$field'] = ${val and 'true' or 'false'};
+// #end
+// #end
+
+// console.log(mode);
+console.log(condfields);
+
+// var ok_view_fields = [];
+// #for field, val in enumerate(condfields.ok_view_fields)
+// ok_view_fields[$field] = '$val';
+// #end
+
+// var ok_new_fields = [];
+// #for field, val in enumerate(condfields.ok_new_fields)
+// ok_new_fields[$field] = '$val';
+// #end
+
+$(function() {
+    if(mode == 'view'){
+        ok_fields = ok_view_fields;
+    } else {
+        ok_fields = ok_new_fields;
+    }
+
+    var field_data = {};
+    for(var i=0;i<ok_fields.length;i++) {
+        var field = ok_fields[i];
+        if(mode == 'view' && field == 'owner') continue;
+
+        field_data[field] = {
+            label: $('label[for=\'field-'+field+'\']').parents('th').html(),
+            input: $('#field-'+field).parents('td').html()
+        };
+    }
+
+    function set_type(t) {
+
+        var col = 1;
+        var table = '';
+        var values = [];
+        for(var i=0;i<ok_fields.length;i++) {
+          var field = ok_fields[i];
+          if(mode == 'view' && field == 'owner') continue;
+          if(condfields[t][field] == 0) continue;
+
+          if(col == 1) {
+		        table += '<tr><th class="col1">';
+		        table += field_data[field].label + '</th>';
+		        table += '<td class="col1">' + field_data[field].input + '</td>';
+          } else {
+            table += '<th class="col2">';
+		        table += field_data[field].label + '</th>';
+		        table += '<td class="col2">' + field_data[field].input + '</td>';
+          }
+
+	        if(col == 1){
+		        col = 2;
+          } else {
+            table += '</tr>';
+            col = 1;
+          }
+
+          // Copy out the value
+          values.push({field:field,value:$('#field-'+field).val()});
+        }
+
+        if(mode == 'new') {
+            //$('#properties tbody').html(table);
+            var n=0;
+            $('#properties tbody tr').each(
+              function() {
+                if(n > 3) {
+                  $(this).remove();
+                }
+                n += 1;
+              });
+            $('#properties tbody').append(table);
+        } else {
+            var n=0;
+            $('#properties tbody tr').each(function() {
+                if(n > 3) {
+                    $(this).remove()
+                }
+                n += 1;
+            });
+            $('#properties tbody').append(table);
+        }
+
+        // Restore the previous values
+        for(var i=0;i<values.length;i++) {
+            $('#field-'+values[i].field).val(values[i].value);
+        }
+    }
+
+    function set_header_type(t) {
+        // Make a dict so I can check containment
+        ok_fields_dict = {};
+        for(var i=0;i<ok_fields.length;i++) {
+            ok_fields_dict[ok_fields[i]] = 1;
+        }
+
+        var elms = [[]];
+        $('table.properties tr').each(function() {
+            $(this).children().each(function() {
+                var attr = {TH: 'id', TD: 'headers'}[this.nodeName];
+                if($(this).attr(attr) != null && condfields[t][$(this).attr(attr).substring(2)] == 1) {
+                    elms[0].push(this);
+                    if(elms[0].length == 4) {
+                        var tmp = $('<tr></tr>');
+                        $.each(elms[0], function(i, e) {
+                            tmp.append(e);
+                        });
+                        elms[0] = tmp;
+                        elms.unshift([]);
+                    }
+                }
+            });
+        });
+        if(elms[0].length == 0) {
+            elms.shift();
+        } else {
+            var tmp = $('<tr></tr>');
+            $.each(elms[0], function(i, e) {
+                tmp.append(e);
+            });
+            elms[0] = tmp;
+        }
+        elms.reverse();
+        $('table.properties').empty();
+        $.each(elms, function(i, row) {
+            $('table.properties').append(row);
+        });
+    }
+
+    if(mode == 'view') {
+        var status_re = /\(\w+ ([^:]+)(: \w+)?\)/;
+        var re_results = status_re.exec($('span.status').text());
+        set_header_type(re_results[1]);
+    }
+
+    set_type($('#field-type').val());
+
+    $('#field-type').change(function() {
+        set_type($(this).val());
+    });
+});
---
+++ 0.11/condfields/web_ui.py
@@ -0,0 +1,129 @@
+# Created by Noah Kantrowitz on 2007-05-05.
+# Copyright (c) 2007 Noah Kantrowitz. All rights reserved.
+
+from trac.core import *
+from trac.config import ListOption, BoolOption
+from trac.web.api import IRequestFilter, IRequestHandler
+from trac.web.chrome import ITemplateProvider, add_script, add_script_data
+from trac.ticket.model import Type
+from trac.ticket.api import TicketSystem
+from trac.util.compat import sorted, set
+from pkg_resources import resource_filename
+import urllib
+
+from pkg_resources import resource_filename
+
+class CondFieldsModule(Component):
+    """A filter to implement conditional fields on the ticket page."""
+
+    implements(IRequestHandler, IRequestFilter, ITemplateProvider) # , ITicketChangeListener)
+    
+    include_std = BoolOption('condfields', 'include_standard', default='true',
+                             doc='Include the standard fields for all types.')
+    
+    forced_fields = set(['type', 'summary', 'reporter', 'description',
+                         'status', 'resolution', 'priority', 'time', 'changetime'])
+
+    def __init__(self):
+        # Initialize ListOption()s for each type.
+        # This makes sure they are visible in IniAdmin, etc
+        self.types = [t.name for t in Type.select(self.env)]
+        for t in self.types:
+            setattr(self.__class__, '%s_fields'%t, ListOption('condfields', t, doc='Fields to include for type "%s"'%t))
+ 
+    def get_fields(self, ticket_type, all_fields, standard_fields):
+        """Retrieve a dictionary mapping each field name to a boolean,
+        indicating whether that field is valid for the specified
+        ticket type."""
+        fields = set(getattr(self, ticket_type+'_fields'))
+        if self.include_std:
+            fields.update(standard_fields)
+        fields.update(self.forced_fields)
+        return dict([
+                (f, f in fields) for f in all_fields
+                ])
+
+    # # ITicketChangeListener methods
+    # def ticket_created(ticket):
+    #     return ()
+ 
+    # def ticket_changed(ticket, comment, author, old_values):
+    #     # get fields for the ticket type:
+        
+    #     ticket['hotness'] = None
+    #     ticket.save_changes('auto-script',None) 
+    #     return ()
+
+    # def ticket_deleted(ticket):
+    #     return ()
+
+    # IRequestHandler methods
+    def match_request(self, req):
+        return req.path_info.startswith('/condfields')
+
+    def process_request(self, req):
+        #self.log.debug("@ process_request")
+        req.send_file(resource_filename(__name__, 'htdocs/condfields.js'), 'text/javascript')
+        return ()
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, origData, content_type):
+        if req.path_info.startswith('/newticket'):
+            mode = 'new'
+        elif req.path_info.startswith('/ticket/'):
+            mode = 'view'
+        else:
+            return template, origData, content_type
+        fieldData = {}
+        fieldData['condfields'] = {}
+
+        all_fields = []
+        standard_fields = set()
+        for f in TicketSystem(self.env).get_ticket_fields():
+            all_fields.append(f['name'])
+            if not f.get('custom'):
+                standard_fields.add(f['name'])
+
+        if 'owner' in all_fields:
+            curr_idx = all_fields.index('owner')
+            if 'cc' in all_fields:
+                insert_idx = all_fields.index('cc')
+            else:
+                insert_idx = len(all_fields)
+            if curr_idx < insert_idx:
+                all_fields.insert(insert_idx, all_fields[curr_idx])
+                del all_fields[curr_idx]
+
+        for t in self.types:
+            fieldData['condfields'][t] = self.get_fields(t, all_fields, standard_fields)
+            # fields = set(getattr(self, t+'_fields'))
+            # if self.include_std:
+            #     fields.update(standard_fields)
+            # fields.update(self.forced_fields)
+            # fieldData['condfields'][t] = dict([
+            #     (f, f in fields) for f in all_fields
+            # ])
+
+        self.log.debug(all_fields)
+        self.log.info(standard_fields)
+
+        fieldData['mode'] = mode
+        fieldData['all_fields'] = list(all_fields)
+        fieldData['ok_view_fields'] = sorted(set(all_fields) - self.forced_fields,
+                                             key=lambda x: all_fields.index(x))
+        fieldData['ok_new_fields'] = sorted((set(all_fields) - self.forced_fields) - set(['owner']),
+                                            key=lambda x: all_fields.index(x))
+
+        add_script_data(req, fieldData)
+        add_script(req, '/condfields.js')
+        return template, origData, content_type
+
+    # ITemplateProvider methods
+    def get_htdocs_dirs(self):
+        return [('condfields', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return [ resource_filename(__name__, 'templates') ]
